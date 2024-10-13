9d3add3d424c5459263b9fd6d27c2d2e121e0457
RoseMontoya <98787013+RoseMontoya@users.noreply.github.com>
2024-10-13 16:19:39 -0700

    finished part three of gatsby tutorial

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="p-32 min-h-vh">
+      <h1>Hello, I&apos;m Rose!</h1>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/layout.js
@@ -0,0 +1,22 @@
+import * as React from 'react'
+import { Link } from 'gatsby'
+import '../styles/global.css'
+
+const Layout = ({ pageTitle, children }) => {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+        </ul>
+      </nav>
+      <main className='p-8'>
+        <h1>{pageTitle}</h1>
+        {children}
+      </main>
+    </div>
+  )
+}
+
+export default Layout
