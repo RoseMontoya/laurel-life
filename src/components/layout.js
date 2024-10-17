2029ce06d0dabde08e59bae21ebb6a1759608253
RoseMontoya <98787013+RoseMontoya@users.noreply.github.com>
2024-10-17 14:29:43 -0700

    trying to get favicon to work

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,14 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
+import { Helmet } from 'react-helmet';
 import '../styles/global.css'
 
 const Layout = ({ pageTitle, children }) => {
   return (
     <div className='bg-lightPink min-h-vh p-8'>
+        <Helmet>
+            <link id='icon' rel="icon" type="image/png" href="/laurel-life-favicon.png" />
+        </Helmet>
       <nav>
         <ul className='flex justify-between align-center'>
           <li><Link to="/">Home</Link></li>
@@ -20,3 +24,9 @@ const Layout = ({ pageTitle, children }) => {
 }
 
 export default Layout
+
+// export const Head = () => (
+//     <>
+//         <link id='icon' rel="icon" type="image/png" href="/laurel-life-favicon.png" />
+//     </>
+// )
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,13 +6,17 @@ import { StaticImage } from 'gatsby-plugin-image'
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
-        <h1 className='flex justify-center'>Welcome to Laurel Life</h1>
+        <div className='flex items-center flex-col'>
+            <h1 >Laurel Life</h1>
+            <h2>It starts with a plan.</h2>
+        </div>
     </Layout>
   )
 }
 
 // You'll learn about this in the next task, just copy it for now
-export const Head = () => <title>Home Page</title>
+// export const Head = () => <title>Home Page</title>
+export { Head } from '../components/layout'
 
 // Step 3: Export your component
 export default IndexPage
